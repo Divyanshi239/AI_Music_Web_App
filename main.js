4b2a4d3191af960b275338348cd5e957736a80b7
Divyanshi239 <75576256+Divyanshi239@users.noreply.github.com>
2021-07-17 09:01:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1 = "";
 song2 = "";
+
+leftWristX = "";
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
 function preload() {
     song1 = loadSound("Butter.mp3");
     song2 = loadSound("No Roots.mp3");
@@ -11,16 +16,27 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Posenet Is Initialized!");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "  Left Wrist Y = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + "  Right Wrist Y = " + rightWristY);
+    }
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
-}
-
-function play1() {
-    song1.play();
-}
-
-function play2() {
-    song2.play();
 }
