f711578688b51b9c307b8aad5e26259966f0bfeb
Divyanshi239 <75576256+Divyanshi239@users.noreply.github.com>
2021-07-21 22:32:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 song1 = "";
 song2 = "";
+ScoreLeftWrist = "";
+SongStatus = "";
 
 leftWristX = "";
 leftWristY = "";
@@ -26,7 +28,7 @@ function modelLoaded() {
 }
 
 function gotPoses(results) {
-    if(results.length > 0) {
+    if (results.length > 0) {
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -34,9 +36,27 @@ function gotPoses(results) {
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("Right Wrist X = " + rightWristX + "  Right Wrist Y = " + rightWristY);
+
+        ScoreLeftWrist = results[0].pose.keypoints[9].score;
     }
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    SongStatus = song1.isPlaying();
+
+    fill("red");
+    stroke("orange");
+
+    if (ScoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+
+
+        if (SongStatus = "false") {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Butter";
+        }
+    }
 }
